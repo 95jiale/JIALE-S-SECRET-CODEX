704cb3138672a76629cdd6769cab9400494d0077
Ng Jun Hao <ngjunhao1996@gmail.com>
2022-02-13 23:20:16 +0800

    Junhao's Changes

--- Easy Difficulty/Array from Permutation.py
+++ Easy Difficulty/Array from Permutation.py
@@ -3,4 +3,4 @@ nums = [5, 0, 1, 2, 3, 4]
 newArray = []  # create an empty list
 for x in range(len(nums)):  # loop through every digit in the list
     newArray.append(nums[nums[x]])
-print(newArray)
+print(newArray) # prints new array
